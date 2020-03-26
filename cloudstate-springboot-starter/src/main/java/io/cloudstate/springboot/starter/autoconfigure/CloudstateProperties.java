c8d8fb90e465af216b67e4876792f70db7daf11c
Adriano Santos <sleipnir@bsd.com.br>
2020-03-26 06:21:32 -0300

    Add some configuration properties

--- cloudstate-springboot-starter/src/main/java/io/cloudstate/springboot/starter/autoconfigure/CloudstateProperties.java
+++ cloudstate-springboot-starter/src/main/java/io/cloudstate/springboot/starter/autoconfigure/CloudstateProperties.java
@@ -3,6 +3,24 @@ package io.cloudstate.springboot.starter.autoconfigure;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "io.cloudstate")
-public class CloudstateProperties {
+public final class CloudstateProperties {
 
+    private String userFunctionInterface;
+    private int userFunctionPort;
+
+    public String getUserFunctionInterface() {
+        return userFunctionInterface;
+    }
+
+    public void setUserFunctionInterface(String userFunctionInterface) {
+        this.userFunctionInterface = userFunctionInterface;
+    }
+
+    public int getUserFunctionPort() {
+        return userFunctionPort;
+    }
+
+    public void setUserFunctionPort(int userFunctionPort) {
+        this.userFunctionPort = userFunctionPort;
+    }
 }
