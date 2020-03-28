f2f8de071951389badbffea4042008252dc40c81
Adriano Santos <sleipnir@bsd.com.br>
2020-03-28 03:02:22 -0300

    NPE fixes and EntityScan test added

--- cloudstate-springboot-starter/src/main/java/io/cloudstate/springboot/starter/internal/CloudstateUtils.java
+++ cloudstate-springboot-starter/src/main/java/io/cloudstate/springboot/starter/internal/CloudstateUtils.java
@@ -2,24 +2,44 @@ package io.cloudstate.springboot.starter.internal;
 
 import io.cloudstate.javasupport.CloudState;
 import io.cloudstate.springboot.starter.internal.scan.CloudstateEntityScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public final class CloudstateUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(CloudstateUtils.class);
 
     public static CloudState register(CloudstateEntityScan entityScan) {
         CloudState cloudState = new CloudState();
-        entityScan.findEntities().forEach(entity -> {
-            switch (entity.getEntityType()) {
-                case EventSourced:
-                    cloudState.registerEventSourcedEntity(entity.getEntityClass(), entity.getDescriptor(), entity.getAdditionalDescriptors());
-                    break;
-                case CRDT:
-                    cloudState.registerCrdtEntity(entity.getEntityClass(), entity.getDescriptor(), entity.getAdditionalDescriptors());
-                    break;
-                default:
-                    throw new IllegalArgumentException(
-                            String.format("Unknown entity type %s", entity.getEntityType()));
-            }
-        });
+        if (Objects.nonNull(entityScan.findEntities()) && !entityScan.findEntities().isEmpty()){
+            entityScan.findEntities().forEach(entity -> {
+
+                if (Objects.nonNull(entity.getDescriptor())) {
+                    switch (entity.getEntityType()) {
+
+                        case EventSourced:
+                            cloudState.registerEventSourcedEntity(
+                                    entity.getEntityClass(),
+                                    entity.getDescriptor(),
+                                    entity.getAdditionalDescriptors());
+                            break;
+                        case CRDT:
+                            cloudState.registerCrdtEntity(
+                                    entity.getEntityClass(),
+                                    entity.getDescriptor(),
+                                    entity.getAdditionalDescriptors());
+                            break;
+                        default:
+                            throw new IllegalArgumentException(
+                                    String.format("Unknown entity type %s", entity.getEntityType()));
+                    }
+                } else {
+                    LOG.warn("Entity '{}' was found but no valid ServiceDescriptor was declared",
+                            entity.getEntityClass().getName());
+                }
+            });
+        }
         return cloudState;
     }
 }
---
+++ cloudstate-springboot-starter/src/test/java/io/cloudstate/springboot/starter/CloudstateEntityScanTest.java
@@ -0,0 +1,39 @@
+package io.cloudstate.springboot.starter;
+
+import io.cloudstate.springboot.starter.autoconfigure.CloudstateAutoConfiguration;
+import io.cloudstate.springboot.starter.autoconfigure.CloudstateProperties;
+import io.cloudstate.springboot.starter.internal.scan.CloudstateEntityScan;
+import io.cloudstate.springboot.starter.internal.scan.Entity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= CloudstateAutoConfiguration.class)
+public class CloudstateEntityScanTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private CloudstateProperties cloudstateProperties;
+
+    @Test
+    public void findEntities_NotBeEmpty(){
+        CloudstateEntityScan scan = new CloudstateEntityScan(context, cloudstateProperties);
+        final List<Entity> entities = scan.findEntities();
+
+        assertNotNull(entities);
+        assertFalse(entities.isEmpty());
+        assertEquals(1, entities.size());
+    }
+}
