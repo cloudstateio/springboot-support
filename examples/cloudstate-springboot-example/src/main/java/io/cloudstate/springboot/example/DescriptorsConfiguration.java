8e09db44c5a58ac3a04ef1d883ab4a5a0c106730
Adriano Santos <sleipnir@bsd.com.br>
2020-04-02 02:42:04 -0300

    Cleanup code

--- examples/cloudstate-springboot-example/src/main/java/io/cloudstate/springboot/example/DescriptorsConfiguration.java
+++ examples/cloudstate-springboot-example/src/main/java/io/cloudstate/springboot/example/DescriptorsConfiguration.java
@@ -1,23 +1,53 @@
 package io.cloudstate.springboot.example;
 
+/**
+ * Spring's bean declaration conventions define that the method name is exactly the name that will be registered in
+ * the Spring injection container as a qualifier. So if you use method names other than those defined in the
+ * Cloudstate Springboot support convention (entity.getSimpleName() + "ServiceDescriptor" for example)
+ * then you will need to use the name property of the @Bean annotation and define the name following these conventions.
+ *
+ * Examples:
+ * If your entity class is called ShoppinCartEntity then you can declare the beans as below:
+ *
+ * @Bean(name = "shoppingCartEntityServiceDescriptor")
+*  public Descriptors.ServiceDescriptor serviceDescriptor() {
+*      return Shoppingcart.getDescriptor().findServiceByName("ShoppingCart");
+*  }
+ *
+ * @Bean(name = "shoppingCartEntityFileDescriptors")
+ * public Descriptors.FileDescriptor[] fileDescriptors() {
+ *      return new Descriptors.FileDescriptor[] {com.example.shoppingcart.persistence.Domain.getDescriptor()};
+ * }
+ *
+ * Or like this:
+ *
+ * @Bean
+ * public Descriptors.ServiceDescriptor shoppingCartEntityServiceDescriptor() {
+ *     return Shoppingcart.getDescriptor().findServiceByName("ShoppingCart");
+ * }
+ *
+ * @Bean
+ * public Descriptors.FileDescriptor[] shoppingCartEntityFileDescriptors() {
+ *     return new Descriptors.FileDescriptor[] {com.example.shoppingcart.persistence.Domain.getDescriptor()};
+ * }
+ *
+ * */
+
 import com.example.shoppingcart.Shoppingcart;
 import com.google.protobuf.Descriptors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DescriptorsConfiguration {
-    private Logger log = LoggerFactory.getLogger(DescriptorsConfiguration.class);
 
-    @Bean(name = "shoppingCartEntityServiceDescriptor")
-    public Descriptors.ServiceDescriptor shoppingCartServiceDescriptor() {
+    @Bean
+    public Descriptors.ServiceDescriptor shoppingCartEntityServiceDescriptor() {
         return Shoppingcart.getDescriptor().findServiceByName("ShoppingCart");
     }
 
-    @Bean(name = "shoppingCartEntityFileDescriptors")
-    public Descriptors.FileDescriptor[] shoppingCartFileDescriptors() {
+    @Bean
+    public Descriptors.FileDescriptor[] shoppingCartEntityFileDescriptors() {
         return new Descriptors.FileDescriptor[]{com.example.shoppingcart.persistence.Domain.getDescriptor()};
     }
 }
