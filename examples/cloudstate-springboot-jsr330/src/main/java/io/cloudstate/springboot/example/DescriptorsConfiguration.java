32aea6375d3a79e55264512910042946d8a8aca6
Adriano Santos <sleipnir@bsd.com.br>
2020-04-29 16:34:07 -0300

    Fix classpath problems

--- examples/cloudstate-springboot-jsr330/src/main/java/io/cloudstate/springboot/example/DescriptorsConfiguration.java
+++ examples/cloudstate-springboot-jsr330/src/main/java/io/cloudstate/springboot/example/DescriptorsConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ShoppingcartConfiguration {
+public class DescriptorsConfiguration {
 
     @Bean("ShoppingCartServiceDescriptor")
     public Descriptors.ServiceDescriptor getDescriptor() {
