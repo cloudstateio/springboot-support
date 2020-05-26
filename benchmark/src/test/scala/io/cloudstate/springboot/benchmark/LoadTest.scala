ddee225bb5d5c0e5bf715da321dc49a2ddd889e7
Adriano Santos <sleipnir@bsd.com.br>
2020-05-26 17:57:33 -0300

    Add benchmark folder

---
+++ benchmark/src/test/scala/io/cloudstate/springboot/benchmark/LoadTest.scala
@@ -0,0 +1,31 @@
+package io.cloudstate.springboot.benchmark
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+class LoadTest extends Simulation {
+
+  val httpProtocol: HttpProtocolBuilder = http
+    .baseUrl("http://localhost:9000")
+
+  object HelloWorldResource {
+    val get: ChainBuilder = exec(http("GetUserCart")
+      .get("/carts/adriano/items"))
+  }
+
+  val myScenario: ScenarioBuilder = scenario("RampUpUsers")
+    .exec(HelloWorldResource.get)
+
+  setUp(myScenario.inject(
+    incrementUsersPerSec(20)
+      .times(5)
+      .eachLevelLasting(5 seconds)
+      .separatedByRampsLasting(5 seconds)
+      .startingFrom(20)
+  )).protocols(httpProtocol)
+    .assertions(global.successfulRequests.percent.is(100))
+}
