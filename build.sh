ee0757a70d58a894292b24e5b0bf7a71be827e2b
Adriano Santos <sleipnir@bsd.com.br>
2020-05-28 15:04:03 -0300

    Add pre compilation phase

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mvn clean
-
+cd cloudstate-springboot-starter && mvn install
 cd examples/cloudstate-springboot-example && mvn protobuf:compile
 cd ../cloudstate-springboot-jsr330 && mvn protobuf:compile
 cd ../../
