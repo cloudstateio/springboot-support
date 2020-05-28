4679a4606c76799899f9dfca89d0696c60d90830
Adriano Santos <sleipnir@bsd.com.br>
2020-05-28 15:32:42 -0300

    Trying pass on travis

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 mvn clean
-cd cloudstate-springboot-starter && mvn install && cd ..
-cd examples/cloudstate-springboot-example && mvn protobuf:compile
-cd ../cloudstate-springboot-jsr330 && mvn protobuf:compile
+cd cloudstate-springboot-starter && mvn install -DskipTests && cd ..
+cd examples
+cd cloudstate-springboot-example
+mvn protobuf:compile
+cd ..
+cd cloudstate-springboot-jsr330
+mvn protobuf:compile
 cd ../../
 
 mvn install
